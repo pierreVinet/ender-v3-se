37c1d14d4285c5519df4852f2117a84ad71c47b1
pierre <pierre.vinet17@gmail.com>
2024-12-03 20:53:52 +0100

    up

--- magnetizer.py
+++ magnetizer.py
@@ -2,12 +2,8 @@ from functions.arduino import move_servo
 import time
 
 def main():
-    move_servo(90)
     time.sleep(2)
-    move_servo(0)
-    time.sleep(2)
+    # move_servo(0)
     move_servo(180)
-    time.sleep(2)
-    move_servo(90)
 
-main
+main()
